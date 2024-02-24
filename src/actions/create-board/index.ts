eb76495c15a4a9c069553e55f05a12e475583bd0
jt01052552295 <jt01052552295@gmail.com>
2024-02-24 19:27:44 +0900

    create-safe-action, useAction

---
+++ src/actions/create-board/index.ts
@@ -0,0 +1,55 @@
+'use server'
+import { auth } from '@/auth'
+import { InputType, ReturnType } from './types'
+import { revalidatePath } from 'next/cache'
+import { db } from '@/lib/db'
+import { CreateBoard } from './schema'
+import { createSafeAction } from '@/lib/create-safe-action'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { user }: any = await auth()
+
+  if (!user) {
+    return {
+      error: 'Unauthorized!',
+    }
+  }
+
+  const { title, image } = data
+
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] = image.split('|')
+
+  if (!imageId || !imageThumbUrl || !imageFullUrl || !imageUserName || !imageLinkHTML) {
+    return {
+      error: 'Missing fields. Failed to create board.',
+    }
+  }
+
+  let board
+
+  const orgId = '1'
+
+  try {
+    // throw Error("test");
+    board = await db.board.create({
+      data: {
+        title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHTML,
+      },
+    })
+  } catch (error) {
+    return {
+      error: 'Failed to create.',
+    }
+  }
+
+  revalidatePath(`/board/${board.id}`)
+  return { data: board }
+}
+
+export const createBoard = createSafeAction(CreateBoard, handler)
--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,14 +1,32 @@
 import { Suspense } from 'react'
 import { auth, signOut } from '@/auth'
-import { create } from '@/actions/createBoard'
+
 import { Button } from '@/components/ui/button'
 import { db } from '@/lib/db'
+import { useAction } from '@/hooks/use-action'
+import { createBoard } from '@/actions/create-board'
 
 const OrganizationIdPage = async () => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data, 'success')
+    },
+    onError: (error) => {
+      console.error(error)
+    },
+  })
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+    const image = '1|2|3|4|5'
+
+    execute({ title, image })
+  }
+
   const boards = await db.board.findMany()
   return (
     <div className="w-full mb-20">
-      <form action={create}>
+      <form action={onSubmit}>
         <input id="title" name="title" required className="border-black border p-1" />
         <Button type="submit">Submit</Button>
       </form>
